4e26057393836305a211a5a3a2d20bcbf68d9d2e
yerinirey <yerin886@naver.com>
2024-06-19 18:50:36 +0900

    practice

---
+++ app/(home)/page.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "HOME",
+};
+
+export default function Page() {
+  return (
+    <div>
+      <h1>HElLo</h1>
+    </div>
+  );
+}
---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "NOT FOUND",
+};
+
+export default function NotFound() {
+  return <h1>Not found</h1>;
+}
