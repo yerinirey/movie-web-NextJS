3761726e3ef6b10091b495d2ddcca72bb4d1d566
yerinirey <yerin886@naver.com>
2024-06-20 02:07:53 +0900

    3.5 Suspense

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,11 +1,24 @@
+import Link from "next/link";
+
 export const metadata = {
   title: "HOME",
 };
+export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
-export default function Page() {
+async function getMovies() {
+  //await new Promise((resolve) => setTimeout(resolve, 1000));
+  return fetch(API_URL).then((response) => response.json());
+}
+
+export default async function HomePage() {
+  const movies = await getMovies();
   return (
     <div>
-      <h1>HElLo</h1>
+      {movies.map((movie) => (
+        <li key={movie.id}>
+          <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
+        </li>
+      ))}
     </div>
   );
 }
--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,8 +1,20 @@
-export default function MovieDetail({
+import { Suspense } from "react";
+import MovieInfo from "../../../../components/movie-info";
+import MovieVideos from "../../../../components/movie-videos";
+
+export default async function MovieDetail({
   params: { id },
 }: {
   params: { id: string };
 }) {
-  console.log(id);
-  return <h1>Movie {id}</h1>;
+  return (
+    <div>
+      <Suspense fallback={<h1>Loading movie info</h1>}>
+        <MovieInfo id={id} />
+      </Suspense>
+      <Suspense fallback={<h1>Loading movie videos</h1>}>
+        <MovieVideos id={id} />
+      </Suspense>
+    </div>
+  );
 }
